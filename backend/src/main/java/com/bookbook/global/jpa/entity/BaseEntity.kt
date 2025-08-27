b742aa0af735431ac9f7d8271cbbbafcdfb44146
heigangbae <j56233145@gmail.com>
2025-08-27 16:54:10 +0900

    baseEntity 코틀린 마이그레이션 2

---
+++ backend/src/main/java/com/bookbook/global/jpa/entity/BaseEntity.kt
@@ -0,0 +1,27 @@
+package com.bookbook.global.jpa.entity
+
+import jakarta.persistence.*
+import lombok.AccessLevel
+import lombok.Getter
+import lombok.Setter
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+// 직접 인스턴스화할 수 없으며, 자식 클래스가 구현하는 추상 메서드를 포함한 클래스
+@MappedSuperclass // JPA에서 부모 클래스의 매핑 정보를 자식 엔티티 클래스에 상속, 이 클래스 자체는 테이블과 매핑되지 않도록 지정
+@EntityListeners(AuditingEntityListener::class) // JPA의 기능을 이용해 엔티티의 생성 및 수정 시간 등을 자동으로 기록
+@Getter
+abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PROTECTED)
+    private val id = 0
+
+    @CreatedDate
+    private val createdDate: LocalDateTime? = null
+
+    @LastModifiedDate
+    private val modifiedDate: LocalDateTime? = null
+}
