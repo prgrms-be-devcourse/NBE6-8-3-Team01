2a33cdc89d668a358f40d3b5e1d268a9256bf443
yoostill <yhs000323@gmail.com>
2025-08-27 17:04:48 +0900

    global/ServiceException 끝

---
+++ backend/src/main/java/com/bookbook/global/exception/ServiceException.kt
@@ -0,0 +1,13 @@
+package com.bookbook.global.exception
+
+import com.bookbook.global.rsdata.RsData
+
+class ServiceException(private val resultCode: String, private val msg: String?) : RuntimeException(
+    resultCode + " : " + msg
+) {
+    // 단순 메시지만으로 생성하는 생성자 추가
+    constructor(msg: String?) : this("400", msg)
+
+    val rsData: RsData<Void?>
+        get() = RsData<Void?>(resultCode, msg, null)
+}
