cc8731f8e71c5359f792b4af21d66e78788654df
meohin <meohin.meo@gmail.com>
2025-08-28 12:51:40 +0900

    refactor: BaseEntity 개선 및 코드 정리
    - id 타입을 int에서 long으로 변경
    - @MappedSuperclass가 있으므로 abstract 선언 불필요
    - Lombok 제거
    - 필드의 접근 제어자 (`protected set`)를 제거

--- backend/src/main/java/com/bookbook/global/jpa/entity/BaseEntity.kt
+++ backend/src/main/java/com/bookbook/global/jpa/entity/BaseEntity.kt
@@ -1,9 +1,6 @@
 package com.bookbook.global.jpa.entity
 
 import jakarta.persistence.*
-import lombok.AccessLevel
-import lombok.Getter
-import lombok.Setter
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
@@ -12,19 +9,14 @@ import java.time.LocalDateTime
 // 직접 인스턴스화할 수 없으며, 자식 클래스가 구현하는 추상 메서드를 포함한 클래스
 @MappedSuperclass // JPA에서 부모 클래스의 매핑 정보를 자식 엔티티 클래스에 상속, 이 클래스 자체는 테이블과 매핑되지 않도록 지정
 @EntityListeners(AuditingEntityListener::class) // JPA의 기능을 이용해 엔티티의 생성 및 수정 시간 등을 자동으로 기록
-@Getter
-abstract class BaseEntity {
+open class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.PROTECTED)
-    var id: Int = 0
-        protected set
+    var id: Long = 0
 
     @CreatedDate
     var createdDate: LocalDateTime? = null
-        protected set
 
     @LastModifiedDate
     var modifiedDate: LocalDateTime? = null
-        protected set
 }
