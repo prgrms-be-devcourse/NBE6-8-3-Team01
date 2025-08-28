1c7365763eb313acdd28b6e5d771cd70c933720c
heigangbae <j56233145@gmail.com>
2025-08-28 10:41:09 +0900

    refactor : UserResponseDto 마이그레이션

---
+++ backend/src/main/java/com/bookbook/domain/user/dto/UserResponseDto.kt
@@ -0,0 +1,32 @@
+package com.bookbook.domain.user.dto
+
+import com.bookbook.domain.user.entity.User
+import com.bookbook.domain.user.enums.Role
+import com.bookbook.domain.user.enums.UserStatus
+import java.time.LocalDateTime
+
+data class UserResponseDto(
+    val id: Long? = null,
+    val username: String,
+    val email: String?,
+    val nickname: String?,
+    val address: String?,
+    val rating: Float,
+    val role: Role,
+    val userStatus: UserStatus,
+    val createAt: LocalDateTime?,
+    val isRegistrationCompleted: Boolean
+) {
+    constructor(user: User) : this(
+        id = user.id,
+        username = user.username,
+        email = user.email,
+        nickname = user.nickname,
+        address = user.address,
+        rating = user.rating,
+        role = user.role,
+        userStatus = user.userStatus,
+        createAt = user.createAt,
+        isRegistrationCompleted = user.isRegistrationCompleted()
+    )
+}
