0bd49a4e3d46637866a059efb46994504dddbaf3
meohin <meohin.meo@gmail.com>
2025-08-28 10:29:47 +0900

    refactor: WishListRepository를 .java > .kt 전환

--- backend/src/main/java/com/bookbook/domain/wishList/repository/WishListRepository.kt
+++ backend/src/main/java/com/bookbook/domain/wishList/repository/WishListRepository.kt
@@ -1,18 +1,13 @@
-package com.bookbook.domain.wishList.repository;
+package com.bookbook.domain.wishList.repository
 
-import com.bookbook.domain.wishList.entity.WishList;
-import com.bookbook.domain.wishList.enums.WishListStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
+import com.bookbook.domain.wishList.entity.WishList
+import com.bookbook.domain.wishList.enums.WishListStatus
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
 @Repository
-public interface WishListRepository extends JpaRepository<WishList, Integer> {
+interface WishListRepository : JpaRepository<WishList, Int> {
+    fun findByUserIdAndStatusOrderByCreatedDateDesc(userId: Long, status: WishListStatus): List<WishList>
 
-    // Status를 포함한 조회 메서드들
-    List<WishList> findByUserIdAndStatusOrderByCreatedDateDesc(Long userId, WishListStatus status);
-
-    Optional<WishList> findByUserIdAndRentIdAndStatus(Long userId, Integer rentId, WishListStatus status);
+    fun findByUserIdAndRentIdAndStatus(userId: Long, rentId: Int, status: WishListStatus): WishList?
 }
