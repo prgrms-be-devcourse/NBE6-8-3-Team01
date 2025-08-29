b839f8843aeaa1a04ebaac7dc163f57a073d6d12
Gooraeng <birdyoon1998@gmail.com>
2025-08-29 14:25:19 +0900

    refactor: ReportAdminController 마이그레이션

--- backend/src/main/java/com/bookbook/domain/report/controller/ReportAdminController.kt
+++ backend/src/main/java/com/bookbook/domain/report/controller/ReportAdminController.kt
@@ -1,38 +1,31 @@
-package com.bookbook.domain.report.controller;
+package com.bookbook.domain.report.controller
 
-import com.bookbook.domain.report.dto.response.ReportDetailResponseDto;
-import com.bookbook.domain.report.dto.response.ReportSimpleResponseDto;
-import com.bookbook.domain.report.enums.ReportStatus;
-import com.bookbook.domain.report.service.ReportService;
-import com.bookbook.global.rsdata.RsData;
-import com.bookbook.global.security.CustomOAuth2User;
-import com.bookbook.global.jpa.dto.response.PageResponseDto;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import com.bookbook.domain.report.dto.response.ReportDetailResponseDto
+import com.bookbook.domain.report.dto.response.ReportSimpleResponseDto
+import com.bookbook.domain.report.enums.ReportStatus
+import com.bookbook.domain.report.service.ReportService
+import com.bookbook.global.jpa.dto.response.PageResponseDto
+import com.bookbook.global.rsdata.RsData
+import com.bookbook.global.security.CustomOAuth2User
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/admin/reports")
-@RequiredArgsConstructor
 @Tag(name = "ReportAdminController", description = "어드민 전용 신고 관리 컨트롤러")
-public class ReportAdminController {
-
-    @Lazy
-    private final ReportService reportService;
-
+class ReportAdminController (
+    private val reportService: ReportService
+){
     /**
      * 신고 글 목록을 가져옵니다.
      *
-     * <p>페이지 번호와 사이즈의 조합, 그리고 신고 처리 상태와
+     *
+     * 페이지 번호와 사이즈의 조합, 그리고 신고 처리 상태와
      * 신고 대상자 ID를 기반으로 필터링된 페이지를 가져올 수 있습니다.
      *
      * @param page 페이지 번호
@@ -43,24 +36,24 @@ public class ReportAdminController {
      */
     @GetMapping
     @Operation(summary = "신고 목록 조회")
-    public ResponseEntity<RsData<PageResponseDto<ReportSimpleResponseDto>>> getReportPage(
-            @RequestParam(defaultValue = "1") Integer page,
-            @RequestParam(defaultValue = "10") Integer size,
-            @RequestParam(required = false) List<ReportStatus> status,
-            @RequestParam(required = false) Long targetUserId
-    ) {
-        Pageable pageable = PageRequest.of(page - 1, size);
+    fun getReportPage(
+        @RequestParam(defaultValue = "1") page: Int,
+        @RequestParam(defaultValue = "10") size: Int,
+        @RequestParam(required = false) status: MutableList<ReportStatus>?,
+        @RequestParam(required = false) targetUserId: Long?
+    ): ResponseEntity<RsData<PageResponseDto<ReportSimpleResponseDto>>> {
+        val pageable: Pageable = PageRequest.of(page - 1, size)
 
-        Page<ReportSimpleResponseDto> reportHistoryPage = reportService.getReportPage(pageable, status, targetUserId);
-        PageResponseDto<ReportSimpleResponseDto> response = new PageResponseDto<>(reportHistoryPage);
+        val reportHistoryPage = reportService.getReportPage(pageable, status, targetUserId)
+        val response = PageResponseDto(reportHistoryPage)
 
         return ResponseEntity.ok(
-                RsData.of(
-                        "200-1",
-                        "%d개의 신고글을 발견했습니다.".formatted(reportHistoryPage.getTotalElements()),
-                        response
-                )
-        );
+            RsData(
+                "200-1",
+                "${response.pageInfo.totalElements}개의 신고글을 발견했습니다.",
+                response
+            )
+        )
     }
 
     /**
@@ -71,16 +64,18 @@ public class ReportAdminController {
      */
     @GetMapping("/{reportId}/review")
     @Operation(summary = "단일 신고 상세 조회")
-    public ResponseEntity<RsData<ReportDetailResponseDto>> getReportDetail(@PathVariable long reportId) {
-        ReportDetailResponseDto reportDetail = reportService.getReportDetail(reportId);
+    fun getReportDetail(
+        @PathVariable reportId: Long
+    ): ResponseEntity<RsData<ReportDetailResponseDto>> {
+        val reportDetail = reportService.getReportDetail(reportId)
 
         return ResponseEntity.ok(
-                RsData.of(
-                        "200-1",
-                        "%d번 신고글 조회 완료".formatted(reportDetail.id),
-                        reportDetail
-                )
-        );
+            RsData(
+                "200-1",
+                "${reportId}번 신고글 조회 완료",
+                reportDetail
+            )
+        )
     }
 
     /**
@@ -90,12 +85,14 @@ public class ReportAdminController {
      */
     @PatchMapping("/{reportId}/process")
     @Operation(summary = "단일 신고 처리 완료")
-    public ResponseEntity<RsData<Void>> processReport(
-            @PathVariable long reportId,
-            @AuthenticationPrincipal CustomOAuth2User adminUser
-    ) {
-        reportService.markReportAsProcessed(reportId, adminUser.getUserId());
+    fun processReport(
+        @PathVariable reportId: Long,
+        @AuthenticationPrincipal adminUser: CustomOAuth2User
+    ): ResponseEntity<RsData<Void>> {
+        reportService.markReportAsProcessed(reportId, adminUser.userId)
 
-        return ResponseEntity.ok(RsData.of("200-1", "%d번 신고가 정상적으로 처리되었습니다.".formatted(reportId)));
+        return ResponseEntity.ok(
+            RsData("200-1", "${reportId}번 신고가 정상적으로 처리되었습니다.")
+        )
     }
 }
