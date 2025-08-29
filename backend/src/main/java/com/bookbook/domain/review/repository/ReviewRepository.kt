6c06ff0f31ee1ae204f612cb9071423552eb1d81
meohin <meohin.meo@gmail.com>
2025-08-29 17:17:55 +0900

    refactor: ReviewRepository를 .java > .kt 전환

--- backend/src/main/java/com/bookbook/domain/review/repository/ReviewRepository.kt
+++ backend/src/main/java/com/bookbook/domain/review/repository/ReviewRepository.kt
@@ -1,21 +1,19 @@
-package com.bookbook.domain.review.repository;
+package com.bookbook.domain.review.repository
 
-import com.bookbook.domain.review.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
+import com.bookbook.domain.review.entity.Review
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    
-    Optional<Review> findByRentIdAndReviewerId(Long rentId, Long reviewerId);
+interface ReviewRepository : JpaRepository<Review, Long> {
+    fun findByRentIdAndReviewerId(rentId: Long, reviewerId: Long): Optional<Review>
 
     @Query("SELECT AVG(r.rating) FROM Review r WHERE r.revieweeId = :revieweeId")
-    Optional<Double> findAverageRatingByRevieweeId(@Param("revieweeId") Long revieweeId);
-    
+    fun findAverageRatingByRevieweeId(@Param("revieweeId") revieweeId: Long): Optional<Double>
+
     // 특정 사용자에 대한 리뷰 개수 조회
-    long countByRevieweeId(Long revieweeId);
+    fun countByRevieweeId(revieweeId: Long): Long
 }
