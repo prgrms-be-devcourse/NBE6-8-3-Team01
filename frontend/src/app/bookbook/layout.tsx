d5cec7182069f340c2d9b10cc0d2abb53e35c829
yoostill <yhs000323@gmail.com>
2025-08-31 01:12:19 +0900

    toast 다시 추가2

--- frontend/src/app/bookbook/layout.tsx
+++ frontend/src/app/bookbook/layout.tsx
@@ -3,7 +3,6 @@ import Bottom from '../components/Bottom';
 import ClientLayout from './ClientLayout';
 import { Suspense } from 'react';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 export default function RootLayout({
   children,
