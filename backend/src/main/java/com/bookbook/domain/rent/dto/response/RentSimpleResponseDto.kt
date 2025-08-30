0871c84e43eadbe27fdc3c46955dfbdb8dc7a11c
Gooraeng <birdyoon1998@gmail.com>
2025-08-30 17:43:23 +0900

    fix: 타입 수정 및 from 삭제

--- backend/src/main/java/com/bookbook/domain/rent/dto/response/RentSimpleResponseDto.kt
+++ backend/src/main/java/com/bookbook/domain/rent/dto/response/RentSimpleResponseDto.kt
@@ -8,29 +8,24 @@ import java.time.LocalDateTime
 // 대여 게시글의 간단한 정보를 담는 DTO.
 data class RentSimpleResponseDto(
     val id: Long,
-    val lenderUserId: Long?,
-    val status: RentStatus?,
-    val bookCondition: String?,
-    val bookTitle: String?,
-    val author: String?,
-    val publisher: String?,
-    val createdDate: LocalDateTime?,
-    val modifiedDate: LocalDateTime?
+    val lenderUserId: Long,
+    val status: RentStatus,
+    val bookCondition: String,
+    val bookTitle: String,
+    val author: String,
+    val publisher: String,
+    val createdDate: LocalDateTime,
+    val modifiedDate: LocalDateTime
 ) {
-    companion object {
-        // Rent Entity로부터 DTO 생성하는 factory 메서드
-        fun from(rent: Rent): RentSimpleResponseDto {
-            return RentSimpleResponseDto(
-                id = rent.id,
-                lenderUserId = rent.lenderUserId,
-                status = rent.rentStatus,
-                bookCondition = rent.bookCondition,
-                bookTitle = rent.bookTitle,
-                author = rent.author,
-                publisher = rent.publisher,
-                createdDate = rent.createdDate,
-                modifiedDate = rent.modifiedDate
-            )
-        }
-    }
+    constructor(rent: Rent) : this(
+        id = rent.id,
+        lenderUserId = rent.lenderUserId,
+        status = rent.rentStatus,
+        bookCondition = rent.bookCondition,
+        bookTitle = rent.bookTitle,
+        author = rent.author,
+        publisher = rent.publisher,
+        createdDate = rent.createdDate,
+        modifiedDate = rent.modifiedDate
+    )
 }
